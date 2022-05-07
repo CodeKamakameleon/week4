8ceeb7c98db9fab08444b1b44e090285b4479167
Kamalani <kamalani@dev-island.org>
2022-05-07 09:05:41 -1000

    my-website

--- my-website/css/index.js
+++ my-website/css/index.js
@@ -1,6 +1,7 @@
-const modalBtn = document.querySelector("#modalBtn");
+const modalBtn = document.querySelector("#contactBtn");
 const modal = document.querySelector(".modal");
 const modalCloseBtn = modal.querySelector(".modal__close-btn");
+// select overlay
 
 function openModal() {
   modal.classList.add("modal_is_open");
@@ -10,5 +11,12 @@ function closeModal() {
   modal.classList.remove("modal_is_open");
 }
 
+function handleEsc(evt) {
+  if (true /*check if the esc was pushed here*/) {
+    closeModal();
+  }
+}
+
 modalBtn.addEventListener("click", openModal, false);
 modalCloseBtn.addEventListener("click", closeModal, false);
+// handle clicking overlay
